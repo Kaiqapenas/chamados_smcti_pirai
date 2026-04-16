90ed9d2031e1b491e3a63d967b3ae639b3460f86
ecolav <151743270+ecolav@users.noreply.github.com>
2026-04-16 10:47:24 -0300

    Model Estoque: Add class MovimentacaoEstoque
    Model Chamado:
    Add class AlteracaoChamado
    Add class ItemChamado

--- apps/estoque/models.py
+++ apps/estoque/models.py
@@ -4,14 +4,15 @@ from django.db import models
 #
 # Rascunho inicial, temporário
 #
+
 class ItemEstoque(models.Model):
     class UnidadeMedida(models.TextChoices):
         UNIDADE = "UN", "Unidade"
         METRO = "M", "Metro"
 
     nome = models.CharField("Nome", max_length=200)
-    quantidade = models.PositiveIntegerField("Quantidade", default=0)
-    quantidade_minima = models.PositiveIntegerField("Quantidade Mínima", default=0)
+    quantidade = models.PositiveIntegerField("Quantidade", default=1)
+    quantidade_minima = models.PositiveIntegerField("Quantidade Mínima", default=1)
     unidade_medida = models.CharField(
         "Unidade de Medida",
         max_length=2,
@@ -84,3 +85,37 @@ class ItemImagem(models.Model):
 
     def __str__(self):
         return f"Imagem de {self.produto.nome}"
+    
+class MovimentacaoEstoque(models.Model):
+    class TipoMovimentacao(models.TextChoices):
+        ENTRADA = "EN", "Entrada"
+        SAIDA = "SA", "Saída"
+
+    item = models.ForeignKey(
+        ItemEstoque,
+        on_delete=models.CASCADE, 
+        related_name="movimentacoes"
+    )
+    
+    protocolo = models.ForeignKey(
+        "chamados.Chamado", 
+        on_delete=models.CASCADE, 
+        related_name="movimentacoes_estoque"
+    )
+    
+    tipo = models.CharField(
+        "Tipo de Movimentação",
+        max_length=2,
+        choices=TipoMovimentacao.choices,
+    )
+    quantidade = models.PositiveIntegerField("Quantidade", default=1)
+    data_movimentacao = models.DateTimeField("Data da Movimentação", auto_now_add=True)
+    observacao = models.TextField("Observação", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Movimentação de Estoque"
+        verbose_name_plural = "Movimentações de Estoque"
+        ordering = ["-data_movimentacao"]
+
+    def __str__(self):
+        return f"{self.get_tipo_display()} - {self.item.nome} ({self.quantidade})"
