f4d5dc3812cec5118a12a67500feded8e61f802a
Érick Ávila <erick.avilasilva@gmail.com>
2026-04-20 14:57:03 -0300

    Adiciona a url raíz em setup.urls redirecionando para core.urls, que renderiza o que seria o index mais futuramente

--- setup/urls.py
+++ setup/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include('apps.core.urls')),
     path("chamados/", include("apps.chamados.urls")),
     path("estoque/", include("apps.estoque.urls")),
 ]
