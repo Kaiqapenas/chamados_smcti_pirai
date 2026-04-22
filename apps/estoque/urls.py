20454c5f90d9c79ecf41557f7cea10466fdb2448
lillyamdl <lillyamaria980@gmail.com>
2026-04-22 09:22:12 -0300

    feat: Atualiza rotas em chamados e estoque: adiciona, edita e remove itens, além de incluir imagens para itens de estoque

--- apps/estoque/urls.py
+++ apps/estoque/urls.py
@@ -8,17 +8,21 @@ urlpatterns = [
     
     # Placeholder para criação de views para criar, editar e excluir Estoque
     path("estoque/adicionar/", EstoqueListView.as_view(), name="adicionar"),
-    path("estoque/editar/<int:id>/", EstoqueListView.as_view(), name="editar"),
-    path("estoque/excluir/<int:id>/", EstoqueListView.as_view(), name="excluir"),
+    path("<int:pk>/", ItemEstoqueDetailView.as_view(), name="detalhe"),
+    path("<int:pk>/", EstoqueListView.as_view(), name="editar"),
+    path("<int:pk>/", EstoqueListView.as_view(), name="excluir"),
     
     # Placeholder para criação de views para criar, editar e excluir categoria de estoque
     path("categoria/adicionar/", EstoqueListView.as_view(), name="adicionar_categoria"),
-    path("categoria/editar/<int:id>/", EstoqueListView.as_view(), name="editar_categoria"),
-    path("categoria/excluir/<int:id>/", EstoqueListView.as_view(), name="excluir_categoria"),
+    path("categoria/<int:pk>/editar/", EstoqueListView.as_view(), name="editar_categoria"),
+    path("categoria/<int:pk>/excluir/", EstoqueListView.as_view(), name="excluir_categoria"),
     
     # Placeholder para criação de views para criar, editar e excluir MovimentaçãoEstoque
     path("movimentacao/adicionar/", EstoqueListView.as_view(), name="adicionar_movimentacao"),
-    path("movimentacao/editar/<int:id>/", EstoqueListView.as_view(), name="editar_movimentacao"),
-    path("movimentacao/excluir/<int:id>/", EstoqueListView.as_view(), name="excluir_movimentacao"),
+    path("movimentacao/<int:pk>/editar/", EstoqueListView.as_view(), name="editar_movimentacao"),
+    path("movimentacao/<int:pk>/excluir/", EstoqueListView.as_view(), name="excluir_movimentacao"),
 
+    # ItemImagem
+    path("<int:item_pk>/imagens/adicionar/", ItemImagemCreateView.as_view(), name="imagem_adicionar"),
+    path("imagens/<int:pk>/remover/", ItemImagemDeleteView.as_view(), name="imagem_remover"),
 ]
