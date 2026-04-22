f404d17ab6f2108dd913b06971e08d2e51357c65
Érick Ávila <erick.avilasilva@gmail.com>
2026-04-22 09:35:50 -0300

    adiciona caminho de static para servir em desenvolvimento

--- setup/urls.py
+++ setup/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -7,3 +9,6 @@ urlpatterns = [
     path("chamados/", include("apps.chamados.urls")),
     path("estoque/", include("apps.estoque.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
