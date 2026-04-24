9d0d4bd4e978a16cfb58e886c613b01d3ef4b3b0
lillyamdl <lillyamaria980@gmail.com>
2026-04-24 12:18:15 -0300

    Adiciona campos ao modelo Chamado e implementa formulários para ItemChamado; atualiza visualizações e URLs

--- apps/chamados/forms.py
+++ apps/chamados/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Chamado
+from .models import Chamado, ItemChamado
 
 class ChamadoForm(forms.ModelForm):
     class Meta:
         model = Chamado
-        fields = ["titulo", "descricao", "status"]
-        
+        fields = ["solicitante", "titulo", "descricao", "urgencia","status"]
+
+class ItemChamadoForm(forms.ModelForm):
+    class Meta:
+        model = ItemChamado
+        fields = ["item", "quantidade"]
